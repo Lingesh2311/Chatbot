a70eeb08ed666547731df3762d012812a61e5517
Lingesh K <lingesh.k.2311@gmail.com>
2019-05-30 00:42:51 +0100

    Add send message

--- chatbot2.py
+++ chatbot2.py
@@ -22,6 +22,19 @@ def respond(message):
     # Return a random statement
     return random.choice(responses["statement"])
 
+# Create templates
+bot_template = "BOT : {0}"
+user_template = "USER : {0}"
+
+# Define a function that sends a message to the bot: send_message
+def send_message(message):
+    # Print user_template including the user_message
+    print(user_template.format(message))
+    # Get the bot's response to the message
+    response = respond(message)
+    # Print the bot template including the bot's response.
+    print(bot_template.format(response))
+
 
 if __name__ == "__main__":
     # Send messages ending in a question mark
