40c45a334513e870f7359fd44d1d4ae6edd2ab4f
Lingesh K <lingesh.k.2311@gmail.com>
2019-05-30 00:40:48 +0100

    Add intelligent question & statement

---
+++ chatbot2.py
@@ -0,0 +1,32 @@
+import random
+
+responses = {
+    "question":[
+        "You tell me!",
+        "I don't know"
+    ]
+    "statement":[
+        ":)",
+        "How's that possible?",
+        "It's not like yesterday then!"
+    ]
+}
+
+
+def respond(message):
+    # Check for a question mark
+    if message[-1] != "?":
+        # Return a random question
+        return random.choice(responses["question"])
+    # Return a random statement
+    return random.choice(responses["statement"])
+
+
+if __name__ == "__main__":
+    # Send messages ending in a question mark
+    send_message("what's today's weather?")
+    send_message("what's today's weather?")
+    
+    # Send messages which don't end with a question mark
+    send_message("I love building chatbots")
+    send_message("I love building chatbots")
